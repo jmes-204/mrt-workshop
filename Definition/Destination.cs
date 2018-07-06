867f349a8c1efdfd5f6ff83137cf1cf64f319d5b
Jmes-204 <prajak@liveup.cc>
2018-07-06 18:41:07 +0700

    update FareService ...

--- Definition/Destination.cs
+++ Definition/Destination.cs
@@ -1,8 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Definition
 {
     public class Destination
     {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Position { get; set; }
+    }
+
+    public static class DestinationManager
+    {
+        public static List<Destination> InitDestinations()
+        {
+            List<Destination> destinations = new List<Destination>();
+            destinations.Add(new Destination
+            {
+                ID = "B9",
+                Name = "พระรามเก้า",
+                Position = 9
+            });
+            destinations.Add(new Destination
+            {
+                ID = "B10",
+                Name = "ศูนย์วัฒนธรรม",
+                Position = 10
+            });
+
+            return destinations;
+        }
+    }
+
+    public class FareRate
+    {
+        public int Distance { get; set; }
+        public int Price { get; set; }
+    }
+
+    public static class FareRateManager
+    {
+        public static List<FareRate> InitFareRates()
+        {
+            List<FareRate> fareRates = new List<FareRate>();
+            fareRates.Add(new FareRate
+            {
+                Distance = 1,
+                Price = 16
+            });
+
+            return fareRates;
+        }
     }
 }
--- Service/FareService.cs
+++ Service/FareService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Definition;
 
 namespace Service
 {
@@ -6,7 +8,37 @@ namespace Service
     {
         public int CalculateFare(string source, string destination)
         {
-            return 16;
+            int destinationPosition = 0;
+            int sourcePosition = 0;
+            int distance = 0;
+
+            List<Destination> destinations = DestinationManager.InitDestinations();
+            foreach (var item in destinations)
+            {
+                if (item.ID == source)
+                {
+                    sourcePosition = item.Position;
+                }
+                if (item.ID == destination)
+                {
+                    destinationPosition = item.Position;
+                }
+            }
+            distance = (destinationPosition - sourcePosition);
+            if (distance < 0)
+            {
+                distance = distance * -1;
+            }
+            List<FareRate> fareRate = FareRateManager.InitFareRates();
+            foreach (var item in fareRate)
+            {
+                if (item.Distance == distance)
+                {
+                    return item.Price;
+                }
+            }
+
+            return 0;
         }
     }
 }
